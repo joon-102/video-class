dec8d5db37f2e5f1b97b3f7fff0af243e1017b88
joon-102 <joon_1@outlook.kr>
2024-06-24 00:20:12 +0900

    Add main page content

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,10 +3,12 @@ import { RouteObject, createBrowserRouter } from 'react-router-dom';
 import Main from './pages/Main';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import Dashboard from './pages/Dashboard'
 
 const routes: RouteObject[] = [
   { path: '/', element: <Main /> },
   { path: '/login', element: <Login /> },
+  { path: '/dashboard', element: <Dashboard /> },
   { path: '*', element: <NotFound /> }
 ];
 
